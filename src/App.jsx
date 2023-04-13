4d30bc8b9620702e0afc64d46e18e6429392dfc6
WAGNER AUGUSTO DE OLIVEIRA <0041938@ifmg.edu.br>
2023-04-13 17:50:38 -0300

    brasileirao

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from "react";
+import { getCampeonato } from "./services/api";
+import './App.css';
+
+function App() {
+  const [brasileirao, setBrasileirao] = useState([]);
+
+  async function fetchData() {
+    const response = await getCampeonato("tabela");
+    setBrasileirao(response);
+  }
+
+  console.log(brasileirao);
+
+  useEffect(() => {
+    if (brasileirao?.length === 0) {
+      fetchData();
+    }
+  }, [brasileirao]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>BRASILEIRÃO 2023</h1>
+        <div className="row">
+          <label>Tabela de Classificação</label>
+        </div>
+      </header>
+
+      {brasileirao.length === 0 ? (
+        <div className="loading">Carregando...</div>
+      ) : (
+        <>
+          <table>
+            <thead>
+              <tr>
+                <th />
+                <th />
+                <th className="txtLeft">Clube</th>
+                <th>Pts</th>
+                <th>Partidas</th>
+                <th>Vitórias</th>
+                <th>Empates</th>
+                <th>Derrotas</th>
+                <th>GM</th>
+                <th>GC</th>
+                <th>SG</th>
+                <th>AP (%)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {brasileirao.map((time, index) => (
+                <tr>
+                  <td>{time.posicao}</td>
+                  <td><img src={time.time.escudo} alt={time.nome} width={30} height={30}></img></td>
+                  <td className="txtLeft">{time.time.nome_popular}</td>
+                  <td>{time.pontos}</td>                
+                  <td>{time.jogos}</td>
+                  <td>{time.vitorias}</td>
+                  <td>{time.empates}</td>
+                  <td>{time.derrotas}</td>
+                  <td>{time.gols_pro}</td>
+                  <td>{time.gols_contra}</td>
+                  <td>{time.saldo_gols}</td>
+                  <td>{time.aproveitamento}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
+
+
+    </div>
+  );
+}
+
+export default App
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'https://api.api-futebol.com.br/v1/',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + import.meta.env.VITE_TESTE_KEY
+    }
+});
+
+const campeonato_id = 10;
+
+export const getCampeonato = async (page) => {
+    try {
+        const response = await api.get(`campeonatos/${campeonato_id}/${page}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
